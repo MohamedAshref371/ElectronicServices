5e3474e37b3c8a32f59ffaf7f8378fdf9968c987
Mohamed Ashref <AshrefKing523@Gmail.com>
2025-09-09 15:52:43 +0300

    new dialog

---
+++ ElectronicServices/UI/ElecListViewDialog.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace ElectronicServices
+{
+    partial class ElecListViewDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ElecListViewDialog));
+            listView1 = new ListView();
+            dateLabel = new Label();
+            datePicker = new DateTimePicker();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // listView1
+            // 
+            listView1.BackColor = Color.Ivory;
+            listView1.Font = new Font("Tahoma", 12F);
+            listView1.ForeColor = Color.Black;
+            listView1.FullRowSelect = true;
+            listView1.GridLines = true;
+            listView1.HeaderStyle = ColumnHeaderStyle.Nonclickable;
+            listView1.Location = new Point(0, 38);
+            listView1.MultiSelect = false;
+            listView1.Name = "listView1";
+            listView1.RightToLeft = RightToLeft.Yes;
+            listView1.RightToLeftLayout = true;
+            listView1.Size = new Size(400, 462);
+            listView1.TabIndex = 0;
+            listView1.UseCompatibleStateImageBehavior = false;
+            listView1.View = View.Details;
+            // 
+            // dateLabel
+            // 
+            dateLabel.AutoSize = true;
+            dateLabel.Font = new Font("Tahoma", 12F);
+            dateLabel.Location = new Point(350, 9);
+            dateLabel.Name = "dateLabel";
+            dateLabel.Size = new Size(50, 19);
+            dateLabel.TabIndex = 2;
+            dateLabel.Text = "التاريخ";
+            // 
+            // datePicker
+            // 
+            datePicker.CustomFormat = "  yyyy - MM MMMM - dd";
+            datePicker.Format = DateTimePickerFormat.Custom;
+            datePicker.Location = new Point(156, 7);
+            datePicker.Name = "datePicker";
+            datePicker.Size = new Size(188, 23);
+            datePicker.TabIndex = 3;
+            datePicker.Value = new DateTime(2025, 9, 9, 15, 46, 0, 0);
+            // 
+            // button1
+            // 
+            button1.Location = new Point(12, 7);
+            button1.Name = "button1";
+            button1.Size = new Size(93, 26);
+            button1.TabIndex = 4;
+            button1.Text = "إعتمد القيم";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // ElecListViewDialog
+            // 
+            AutoScaleDimensions = new SizeF(96F, 96F);
+            AutoScaleMode = AutoScaleMode.Dpi;
+            ClientSize = new Size(400, 500);
+            Controls.Add(button1);
+            Controls.Add(datePicker);
+            Controls.Add(dateLabel);
+            Controls.Add(listView1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "ElecListViewDialog";
+            StartPosition = FormStartPosition.CenterParent;
+            Text = "مساعد الحقول";
+            ResumeLayout(false);
+            PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label dateLabel;
+        private DateTimePicker datePicker;
+        private Button button1;
+    }
+}
---
+++ ElectronicServices/UI/ElecListViewDialog.cs
@@ -0,0 +1,86 @@
+﻿
+namespace ElectronicServices
+{
+    public partial class ElecListViewDialog : Form
+    {
+        public int SelectedIndex { get; private set; } = -1;
+
+        public ElecListViewDialog()
+        {
+            InitializeComponent();
+
+            if (data.Length >= 17)
+            {
+                ClientSize = new Size(ClientSize.Width + 20, ClientSize.Height);
+                listView1.ClientSize = new Size(listView1.ClientSize.Width + 20, listView1.ClientSize.Height);
+            }
+
+            listView1.Columns.Add("تطبيق الدفع الإلكتروني", 320, HorizontalAlignment.Center);
+            listView1.Columns.Add("الرصيد", listView1.ClientSize.Width - 321, HorizontalAlignment.Center);
+
+            ListViewItem item; int total = 0;
+            for (int i = 0; i < data.Length; i++)
+            {
+                item = new ListViewItem(data[i].Text);
+                total += data[i].Count;
+                item.SubItems.Add(data[i].Count.ToString());
+                listView1.Items.Add(item);
+            }
+
+            listView1.DoubleClick += (s, e) => ConfirmSelection();
+
+            item = new ListViewItem("المجموع") { BackColor = Color.FromArgb(220, 255, 220) };
+            item.SubItems.Add(total.ToString());
+            listView1.Items.Add(item);
+            listView1.ItemSelectionChanged += (s, e) =>
+            {
+                if (e.Item == item && e.IsSelected)
+                    e.Item.Selected = false;
+            };
+
+            ListViewItem item2 = new ("الفرق بين اليوم السابق") { BackColor = Color.FromArgb(220, 255, 220) };
+            item2.SubItems.Add(/*total*/.ToString());
+            listView1.Items.Add(item2);
+            listView1.ItemSelectionChanged += (s, e) =>
+            {
+                if (e.Item == item2 && e.IsSelected)
+                    e.Item.Selected = false;
+            };
+        }
+
+        private void ConfirmSelection()
+        {
+            if (listView1.SelectedIndices.Count > 0)
+            {
+                SelectedIndex = listView1.SelectedIndices[0];
+                DialogResult = DialogResult.OK;
+                Close();
+            }
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            string query = textBox1.Text.Trim();
+
+            if (string.IsNullOrEmpty(query))
+            {
+                listView1.SelectedItems.Clear();
+                return;
+            }
+
+            foreach (ListViewItem item in listView1.Items)
+            {
+                if (item.Text.IndexOf(query, StringComparison.OrdinalIgnoreCase) >= 0)
+                {
+                    item.Selected = true;
+                    item.Focused = true;
+                    item.EnsureVisible();
+                    break;
+                }
+                else
+                    item.Selected = false;
+            }
+        }
+
+    }
+}
