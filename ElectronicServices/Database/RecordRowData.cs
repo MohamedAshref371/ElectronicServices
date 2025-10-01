b6b27d7d91843700f0ccc92512fc0e8a22691a27
Mohamed Ashref <AshrefKing523@Gmail.com>
2025-10-01 18:49:17 +0300

    update

--- ElectronicServices/Database/RecordRowData.cs
+++ ElectronicServices/Database/RecordRowData.cs
@@ -11,5 +11,7 @@ namespace ElectronicServices
         public float Deposit;
         public float Balance;
         public string Comment;
+
+        public override string ToString() => $"'{Phone}', '{Date}', {WithdrawalRemaining}, {DepositRemaining}, {Withdrawal}, {Deposit}, {Balance}, '{Comment}'";
     }
 }
