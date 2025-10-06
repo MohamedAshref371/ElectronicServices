e3698690a1250bdf11adecc26df0a49d682580ee
Mohamed Ashref <AshrefKing523@Gmail.com>
2025-10-06 17:21:40 +0300

    org

---
+++ ElectronicServices/UI/MessageBoxIconV2.cs
@@ -0,0 +1,14 @@
+﻿
+namespace ElectronicServices
+{
+    public enum MessageBoxIconV2
+    {
+        None = 0,
+        Error = 16,
+        Question = 32,
+        Warning = 48,
+        Information = 64,
+        Correct = 80,
+        Delete = 96,
+    }
+}
--- ElectronicServices/UI/MessageForm.cs
+++ ElectronicServices/UI/MessageForm.cs
@@ -68,16 +68,5 @@ namespace ElectronicServices
         {
 
         }
-
-        public enum MessageBoxIconV2
-        {
-            None = 0,
-            Error = 16,
-            Question = 32,
-            Warning = 48,
-            Information = 64,
-            Correct = 80,
-            Delete = 96,
-        }
     }
 }
